f670f7d5acf659f1a027f87333aa4ff3cbf2c696
Ryan Lenard Dy <144010659+ryanldy@users.noreply.github.com>
2025-02-21 14:35:30 +0800

    Added CLICKHOUSE_SKIP_USER_SETUP=1
    From https://hub.docker.com/_/clickhouse: 
    
    The user default has disabled network access by default in the case none of CLICKHOUSE_USER, CLICKHOUSE_PASSWORD, or CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT are set.
    
    There's a way to make default user insecurely available by setting environment variable CLICKHOUSE_SKIP_USER_SETUP to 1

--- run-clickhouse.sh
+++ run-clickhouse.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 8123:8123 -p 9000:9000 --name clickhouse-test --ulimit nofile=262144:262144 --rm clickhouse
+docker run -d -p 8123:8123 -p 9000:9000 --name clickhouse-test --ulimit nofile=262144:262144 -e CLICKHOUSE_SKIP_USER_SETUP=1 --rm clickhouse
