b49a84cc33213fc1d5ccd95458f02cc4c3812de2
Anton Pirker <anton.pirker@sentry.io>
2025-03-28 10:15:36 +0100

    Give it more time

--- run-clickhouse.sh
+++ run-clickhouse.sh
@@ -2,7 +2,7 @@
 
 docker run -d -e CLICKHOUSE_SKIP_USER_SETUP=1 -p 8123:8123 -p 9000:9000 --name clickhouse-test --ulimit nofile=262144:262144 --rm clickhouse
 
-sleep 3
+sleep 10
 
 echo "--- Containers ---"
 docker ps
